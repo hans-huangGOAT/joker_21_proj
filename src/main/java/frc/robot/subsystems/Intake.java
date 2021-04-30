8cb28df39bf5a968dc658106e549a548d6c762b5
Hans <fb_hans@outlook.com>
2021-04-30 22:22:06 +0800

    intake subsystem update

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConst;
 
 public class Intake extends SubsystemBase {
   private final WPI_VictorSPX sucker_motor;
@@ -15,8 +16,28 @@ public class Intake extends SubsystemBase {
 
   /** Creates a new Intake. */
   public Intake() {
-    sucker_motor = new WPI_VictorSPX();
-    expan_solen = new Solenoid();
+    sucker_motor = new WPI_VictorSPX(IntakeConst.SUCKER_MOTOR_PORT);
+    expan_solen = new Solenoid(IntakeConst.EXPAN_SOLEN_PORT);
+  }
+
+  public void sucker_in(double sucking_speed) {
+    sucker_motor.set(sucking_speed);
+  }
+
+  public void sucker_out(double out_speed) {
+    sucker_motor.set(out_speed);
+  }
+
+  public void sucker_stop() {
+    sucker_motor.set(0);
+  }
+
+  public void push_out() {
+    expan_solen.set(IntakeConst.EXPAN_SOLEN_PUSH_OUT_MODE);
+  }
+
+  public void take_back() {
+    expan_solen.set(IntakeConst.EXPAN_SOLEN_TAKE_BACK_MODE);
   }
 
   @Override
